e29238a5304ec5159cd6838b08f6bd166b1f9010
Alina <linalevel5@gmail.com>
2023-01-19 18:53:52 +0300

    Initial Commit

---
+++ ColorizedAppSwiftUI/ColorizedAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSwiftUIApp.swift
+//  ColorizedAppSwiftUI
+//
+//  Created by MAcbook on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
